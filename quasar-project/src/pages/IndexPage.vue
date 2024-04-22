1ffa266fe3c6ec4bd18040117643350f7f0e2869
igor1289 <igor1289@gmail.com>
2024-04-22 22:11:14 +0300

    Первичный коммит

---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="q-pa-md">
+    <div class="text-h1">
+      The Heading
+    </div>
+    <q-btn>My Button</q-btn>
+  </div>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+</script>
